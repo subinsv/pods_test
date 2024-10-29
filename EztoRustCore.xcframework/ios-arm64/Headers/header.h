86e0412a44c22070b4720679e2ca516a42cda07e
Subin S V <subin.selvaraj@grootan.com>
2024-10-29 17:03:19 +0530

    test framework

--- EztoRustCore.xcframework/ios-arm64/Headers/header.h
+++ EztoRustCore.xcframework/ios-arm64/Headers/header.h
@@ -1,11 +1,6 @@
-#include <cstdarg>
-#include <cstdint>
-#include <cstdlib>
-#include <ostream>
-#include <new>
-
-extern "C" {
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdlib.h>
 
 uintptr_t add(uintptr_t left, uintptr_t right);
-
-}  // extern "C"
